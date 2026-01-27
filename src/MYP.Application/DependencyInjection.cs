52143ce0ec78da9381e3a5bc79ffb3316bb92f97
roluminox <roluminox@gmail.com>
2026-01-27 18:39:16 +0100

    feat(application): add MediatR pipeline behaviors
    - Add ValidationBehavior for automatic FluentValidation
    - Add LoggingBehavior for request logging and slow query detection
    - Register behaviors in MediatR pipeline

--- src/MYP.Application/DependencyInjection.cs
+++ src/MYP.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using MYP.Application.Common.Behaviors;
 
 namespace MYP.Application;
 
@@ -11,7 +13,11 @@ public static class DependencyInjection
         services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
 
         services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
